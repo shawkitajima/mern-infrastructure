0d5328683c10aa2167d02cd3aae16143030efccc
Shaw Kitajima <shawkitajima@gmail.com>
2021-05-17 12:05:38 -0700

    add server side authentication and authorization middleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
 
+// require the authorization middleware function to protect routes
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
+
 // POST /api/users
 router.post('/', usersCtrl.create);
 router.post('/login', usersCtrl.login);
